f55d296bc92ac8199c6cc39552f70721d03e4976
NUANU5252 <84632916+NUANU5252@users.noreply.github.com>
2021-11-05 17:14:53 +0900

    Update crash_check.py
    충돌체크 함수 2 테스트중

--- crash_check.py
+++ crash_check.py
@@ -44,5 +44,24 @@ def collision_check(x1, y1, x2, y2, x3, y3, x4, y4):
         return 0
 
 
+def collision_check_2(x1, y1, x2, y2, x3, y3, x4, y4):
+    b1, b2 = x1 < x3, x4 < x2
+    b3, b4 = y1 < y3, y4 < y2
+
+    b5, b6 = x3 < x1, x2 < x4
+    b7, b8 = y3 < y1, y2 < y4
+
+    value1 = (b1 or b2) and (b3 or b4)
+    value2 = (b5 or b6) and (b7 or b8)
+
+    # 아래의 경우 속도가 너무 빠른 경우
+    value3 = (b1 or b2) and (b7 or b8)
+    value4 = (b5 or b6) and (b3 or b4)
+
+    return value1 or value2 or value3 or value4
+
+
+
+
 if __name__ == '__main__':
   pass
