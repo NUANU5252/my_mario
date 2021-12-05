61f2682209227a4c816ecab564d39771b691345d
NUANU5252 <84632916+NUANU5252@users.noreply.github.com>
2021-12-06 07:38:22 +0900

    화면 크기 변경

--- mygame.py
+++ mygame.py
@@ -5,7 +5,7 @@ import start_state
 import main_state
 
 cavers_width = 48*16
-cavers_height = 600
+cavers_height = 48*12
 pico2d.open_canvas(cavers_width, cavers_height)
 game_framework.run(start_state)
 # game_framework.run(main_state)
